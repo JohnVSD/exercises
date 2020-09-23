076419378954712eb797c2d1e482a283bb3a4228
王春伟 <wangchunwei@MacBook-Pro-2.local>
2020-09-23 10:24:47 +0800

    feat：新增函数式练习

---
+++ fp/index.js
@@ -0,0 +1,17 @@
+
+function ajax(url, data, callback) {
+  // ...
+  console.warn('ajax 请求：', url, data, callback);
+}
+
+// ajax的偏函数。是一个和减少函数参数个数的过程。
+function getPerson(data, cb) {
+  ajax("http://some.api/person", data, cb);
+}
+
+// 这就是函数式。
+function partial(fn, ...presetArgs) {
+  return function partiallyApplied(...laterArgs){
+    return fn( ...presetArgs, ...laterArgs);
+  }
+}
